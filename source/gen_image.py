dad0510c85d42880d990acf3907d171e86eaa1f8
Leonid Ugadiarov <ugadiarov.la@phystech.edu>
2025-03-31 23:54:24 +0300

    Add original image to visualization

--- source/gen_image.py
+++ source/gen_image.py
@@ -138,4 +138,4 @@ def get_image(
     s_p = to_one_hot(preds[0], num_classes=n_clusters).unsqueeze(1)
     attn_p = s_img * s_p + (1 - s_p)
 
-    return make_grid(attn_p, nrow=attn_p.shape[0], pad_value=0.5).cpu()
+    return make_grid(torch.cat([s_img, attn_p]), nrow=attn_p.shape[0] + 1, pad_value=0.5).cpu()
